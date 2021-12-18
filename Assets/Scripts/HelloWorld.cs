f60e2864eb6c0216e2e3a793d26286131f25c4d8
Daigowo <die321d.21@gmail.com>
2021-12-18 00:14:37 -0300

    Se crea iddle animation y se modifica el archivo de prueba

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hola Mundo!");
+        print("Hola Mundo! version 2.0");
     }
 
     // Update is called once per frame
