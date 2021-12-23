803aff1302ba9e68fdecbd1ce57918186943441a
Daigowo <die321d.21@gmail.com>
2021-12-23 13:22:32 -0300

    Ajuste de parametros, y salto agregado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,23 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Jumping
+    private bool isGrounded;
+    public Transform feetPos;
+    private float checkRadius;
+    public float jumpForce = 5f;
+
+    // Movement
     public float movement_speed = 3f;
-    private Rigidbody2D player_physics;
     private Vector2 movement;
+    private float horizontalAxis;
+    public LayerMask whatIsGround;
+
+    // Physics
+    private Rigidbody2D player_physics;
+
+    //sprite & animations
     private bool facingRight = true;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        player_physics = this.GetComponent<Rigidbody2D>();
+        player_physics = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalAxis = Input.GetAxisRaw("Horizontal");
-        movement = new Vector2(horizontalAxis, player_physics.velocity.y);
+        horizontalAxis = Input.GetAxisRaw("Horizontal"); // obtain values of where is moving, -1 left, 0 idle, 1 right
 
+        // flip sprite
         if (horizontalAxis > 0 && !facingRight)
         {
             Flip();
@@ -30,21 +45,29 @@ public class PlayerController : MonoBehaviour
             // ... flip the player.
             Flip();
         }
+
+        // Animation
+        animator.SetFloat("playerSpeed", Mathf.Abs(horizontalAxis));
+
+        // Jump
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space)) 
+        {
+            player_physics.velocity = Vector2.up * jumpForce;
+        }
     }
 
     private void FixedUpdate()
     {
-        moveCharacter(movement);
+        player_physics.velocity = new Vector2(horizontalAxis * movement_speed, player_physics.velocity.y);
     }
 
-    private void moveCharacter(Vector2 direction)
-    {
-        player_physics.velocity = direction * movement_speed;
-    }
+   
 
     private void Flip()
     {
-        // Switch the way the player is labelled as facing.
+        // Switch the way the player is labeled as facing.
         facingRight = !facingRight;
 
         // Multiply the player's x local scale by -1.
